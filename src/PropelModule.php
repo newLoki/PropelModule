710e90091b5d2429f98121e256c6269315f54254
Alberto Assmann <alberto.assmann@project-a.com>
2014-11-17 11:42:09 +0100

    implement database test function

--- src/PropelModule.php
+++ src/PropelModule.php
@@ -3,6 +3,7 @@
 namespace Codeception\Module;
 
 use Codeception\Module;
+use Codeception\Exception\Module as ModuleException;
 
 class PropelModule extends Module
 {
@@ -27,5 +28,77 @@ class PropelModule extends Module
     public function _afterSuite()
     {
         $this->connection->rollBack();
+    }
+
+    /**
+     * @param string $table
+     * @param array|string $selectColumns
+     * @param array $criteria
+     * @throws ModuleException
+     */
+    public function seeInDatabase($table, $selectColumns, array $criteria)
+    {
+        $criteriaColumns = array_keys($criteria);
+        $criteriaConditions = array_values($criteria);
+
+        $where = '';
+
+        for ($i = 0; $i < count($criteriaColumns); $i++) {
+            if ($i > 1) {
+                $where .= ' AND ';
+            }
+
+            $where .= $criteriaColumns[$i] . ' = :' . $i;
+        }
+
+        $sql = sprintf(
+            'SELECT %s FROM %s WHERE %s',
+            is_array($selectColumns) ? implode(', ', $selectColumns) : $selectColumns,
+            $table,
+            $where
+        );
+        $query = $this->connection->query($sql);
+        $this->debugSection('Query: ', $sql);
+        $this->debugSection('Values; ', implode(', ', $criteriaConditions));
+
+        for ($i = 0; $i < count($criteriaConditions); $i++) {
+            $query->bindValue(
+                (string) $i,
+                $criteriaConditions[$i],
+                $this->guessParamType($criteriaConditions[$i])
+            );
+        }
+    }
+
+    /**
+     * @param mixed $param
+     * @return int
+     * @throws ModuleException
+     */
+    protected function guessParamType($param)
+    {
+        if (!is_scalar($param)) {
+            throw new ModuleException(
+                __CLASS__,
+                'Only scalar parameters are allowed for propel conditions'
+            );
+        }
+
+        switch ($param) {
+            case is_int($param):
+                $type = \PDO::PARAM_INT;
+                break;
+            case is_bool($param):
+                $type = \PDO::PARAM_BOOL;
+                break;
+            case is_float($param):
+                $type = \PDO::PARAM_STR;
+                break;
+            default:
+                $type = \PDO::PARAM_STR;
+                break;
+        }
+
+        return $type;
     }
 }
